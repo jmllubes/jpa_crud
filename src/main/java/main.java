dd7a8095618d8d154e86611db6f2770cd549a6f6
Josep Maria Llubes Cliville <jmllubes@gmail.com>
2023-11-17 12:11:17 +0100

    insertar preguntes i tot

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,6 +2,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.Query;
+import model.Pregunta;
 import model.Respuesta;
 import model.Tema;
 import model.TemaHasPregunta;
@@ -18,28 +19,35 @@ public class main {
         EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "default" );
         EntityManager entitymanager = emfactory.createEntityManager();
         entitymanager.getTransaction().begin();
-        Query query = entitymanager.createQuery("SELECT t FROM Tema t");
-        List<Tema> list = query.getResultList();
-        for (Tema t : list) {
-            System.out.println(t.toString());
-        }
-        System.out.println("--------------------------------------------------");
-        System.out.println("Tria un idtema:");
-        Scanner sc = new Scanner(System.in);
-        int idTema = sc.nextInt();
-        Tema tema = entitymanager.find(Tema.class, idTema);
-        Collection<TemaHasPregunta> temaHasPreguntas = tema.getTemaHasPreguntasByIdtema();
-        for (TemaHasPregunta thp : temaHasPreguntas) {
-            System.out.println(thp.getPreguntaByPreguntaIdpregunta().getPregunta());
-            System.out.println("--------------------------------------------------");
-            Collection<Respuesta> llr = thp.getPreguntaByPreguntaIdpregunta().getRespuestasByIdpregunta();
-            for (Respuesta r : llr) {
-                System.out.println(r.toString());
+        Tema tema = new Tema();
+        tema.setIdtema(100);
+        tema.setTema("Tema 100");
+        entitymanager.persist(tema);
 
-            }
-            System.out.println("--------------------------------------------------");
+        Pregunta pregunta = new Pregunta();
+        pregunta.setIdpregunta(100);
+        pregunta.setPregunta("Pregunta 100");
+        pregunta.setCorrecta(1);
+        entitymanager.persist(pregunta);
 
-        }
+        TemaHasPregunta temaHasPregunta = new TemaHasPregunta();
+        temaHasPregunta.setTemaIdtema(100);
+        temaHasPregunta.setPreguntaIdpregunta(100);
+        entitymanager.persist(temaHasPregunta);
+
+        Respuesta respuesta = new Respuesta();
+        respuesta.setPreguntaIdpregunta(100);
+        respuesta.setIdrespuesta(100);
+        respuesta.setRespuesta("Respuesta 100");
+        entitymanager.persist(respuesta);
+
+        Respuesta respuesta2 = new Respuesta();
+        respuesta2.setPreguntaIdpregunta(100);
+        respuesta2.setIdrespuesta(101);
+        respuesta2.setRespuesta("Respuesta 101");
+        entitymanager.persist(respuesta2);
+
+
         entitymanager.getTransaction().commit();
         entitymanager.close();
         emfactory.close();
